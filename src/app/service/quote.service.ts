a60cfd12c1b2595ca6f42970f78cae12c02d812b
Iwona007 <iwonka.adamkiewicz@gmail.com>
2020-04-28 20:37:38 +0200

    url change

--- src/app/service/quote.service.ts
+++ src/app/service/quote.service.ts
@@ -1,27 +1,35 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Image} from '../model/image';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content_Type': 'application/json'
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuoteService {
 
-  apiUrl: string = 'https://modol12.herokuapp.com/';
+  apiUrl: string = 'https://modol12.herokuapp.com/ocr/';
+
+  // apiUrl: string = 'http://localhost:8080/ocr/';
 
   constructor(private httpClient: HttpClient) {
   }
 
   getAll(): Observable<Array<Image>> {
-    return this.httpClient.get<Array<Image>>(this.apiUrl + 'all');
+    return this.httpClient.get<Array<Image>>(this.apiUrl + 'all', httpOptions);
   }
 
   addImage(image: Image): Observable<Image> {
-    return this.httpClient.post<Image>(this.apiUrl + 'add', image);
+    return this.httpClient.post<Image>(this.apiUrl + 'add', image, httpOptions);
   }
 
   delete(id: number): Observable<Image> {
-    return this.httpClient.delete<Image>(this.apiUrl + 'all/' + id);
+    return this.httpClient.delete<Image>(this.apiUrl + 'all/' + id, httpOptions);
   }
 }
