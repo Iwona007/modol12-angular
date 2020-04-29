7f31fde8d3003e5694db2c279c71c986bd7c9dd5
Iwona007 <iwonka.adamkiewicz@gmail.com>
2020-04-30 01:51:23 +0200

    proxy

--- src/app/service/quote.service.ts
+++ src/app/service/quote.service.ts
@@ -14,22 +14,22 @@ const httpOptions = {
 })
 export class QuoteService {
 
-  apiUrl: string = 'https://modol12.herokuapp.com/ocr/';
-  // apiUrl: string = 'http://localhost:8080/ocr/';
+  // apiUrl: string = 'https://modol12.herokuapp.com/';
+  apiUrl: string = 'http://localhost:8080/ocr/';
 
   constructor(private httpClient: HttpClient) {
   }
 
   getAll(): Observable<Array<Image>> {
-    return this.httpClient.get<Array<Image>>(this.apiUrl + 'all', httpOptions);
+    return this.httpClient.get<Array<Image>>(this.apiUrl + '/ocr/all', httpOptions);
   }
 
   addImage(image: Image): Observable<Image> {
-    return this.httpClient.post<Image>(this.apiUrl + 'add', image, httpOptions);
+    return this.httpClient.post<Image>(this.apiUrl + '/ocr/add', image, httpOptions);
   }
 
   delete(id: number): Observable<Image> {
-    const url = `${this.apiUrl + 'all/'}/${id}`;
+    const url = `${this.apiUrl + '/ocr/all/'}/${id}`;
     return this.httpClient.delete<Image>(url, httpOptions);
   }
 }
