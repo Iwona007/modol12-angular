db67448040f087727aee5ab87fb0bf9bb52b6da6
Iwona007 <iwonka.adamkiewicz@gmail.com>
2020-04-30 20:45:00 +0200

    server.js, npm install core-js@latest zone.js@latest --save

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(__dirname + '/dist'));
+app.listen(process.env.PORT || 8080);
+
+// ??PathLocationStrategy
+
+app.get('/[^\.]+$', function (req, res) {
+  res.set('Content-Type', 'text/html')
+    .sendFile(path.join(__dirname + 'dist/index.html'));
+});
+app.listen(app.get('port'), function () {
+  console.log('Console listening!' + app.get('port'));
+});
