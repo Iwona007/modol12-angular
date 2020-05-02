9b2e6ff271bfdbae71b376b4b329085d69a0bf0c
Iwona007 <iwonka.adamkiewicz@gmail.com>
2020-05-02 14:00:04 +0200

    server.js

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/model12-angular'));
 app.listen(process.env.PORT || 8080);
 
 // ??PathLocationStrategy
 
 app.get('/[^\.]+$', function (req, res) {
   res.set('Content-Type', 'text/html')
-    .sendFile(path.join(__dirname + 'dist/index.html'));
+    .sendFile(path.join(__dirname + 'dist/model12-angular/index.html'));
 });
 app.listen(app.get('port'), function () {
   console.log('Console listening!' + app.get('port'));
