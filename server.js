dc8f2970832c53cd3b3a1debd38597f77840aa52
Iwona007 <iwonka.adamkiewicz@gmail.com>
2020-05-02 03:04:49 +0200

    server

--- server.js
+++ server.js
@@ -14,3 +14,20 @@ app.get('/[^\.]+$', function (req, res) {
 app.listen(app.get('port'), function () {
   console.log('Console listening!' + app.get('port'));
 });
+
+//
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+//
+// app.use(express.static(__dirname + '/dist/model12v2-angular'));
+//
+// app.listen(process.env.PORT || 8080);
+//
+// // ??PathLocationStrategy
+// app.get('/*', function (req, res) {
+//   res.sendFile(path.join(__dirname, '/dist/model12v2-angular/index.html'));
+// });
+//
+//
+// console.log('Console listening!' + app.get('port'));
