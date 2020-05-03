56208b28a5fdbdef0ce38d58469280e3b3b883aa
Iwona007 <iwonka.adamkiewicz@gmail.com>
2020-05-04 00:32:25 +0200

    change proxy

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,13 +1,13 @@
-const PROXY_CONFIG = [
-  {
-    context: [
-      "/ocr/all",
-      "/ocr/all/{id}",
-      "/ocr/add"
-    ],
-    target: "https://modol12.herokuapp.com",
-    secure: false
-  }
-];
-
-module.exports = PROXY_CONFIG;
+// const PROXY_CONFIG = [
+//   {
+//     context: [
+//       "/ocr/all",
+//       "/ocr/all/{id}",
+//       "/ocr/add"
+//     ],
+//     target: "https://modol12.herokuapp.com",
+//     secure: false
+//   }
+// ];
+//
+// module.exports = PROXY_CONFIG;
